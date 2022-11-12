dce1cf2d77499357b3458a0d6991c83ecb11c805
felipe <felipebomfim4161@gmail.com>
2022-11-12 19:12:38 -0300

    Learning to manipulate arrays in PHP

---
+++ htdocs-copy/php-intermediario/switch.php
@@ -0,0 +1,20 @@
+<?php
+    $nome = 'Joao';
+
+    // if ($nome == 'Felipe'){
+    //     echo "Seu nome é Felipe!";
+    // } else {
+    //     echo "Seu nome não é Felipe!";
+    //     echo "Seu nome é $nome";
+    // }
+
+    switch($nome){
+        case 'Joao':
+            echo 'Meu nome é joao <br/>';
+            break;
+        case 'Felipe':
+            echo 'Meu nome é Felipe';
+            break;
+            
+    }
+?>
